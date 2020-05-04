2b9fb893e4ca292c659ac894df0fe623150e7816
u61 <utkukopuz.nl@outlook.com>
2020-05-04 20:47:14 +0200

    basic introduction to variables and variable overwriting

--- main.c
+++ main.c
@@ -3,9 +3,15 @@
 
 int main()
 {
-  printf("    /|\n");
-  printf("   / |\n");
-  printf("  /  |\n");
-  printf(" /___|\n");
+  char characterName[] = "John";
+  int characterAge = 35;
+
+  printf("There once was a man named %s\n", characterName);
+  printf("he was %d years old.\n", characterAge);
+
+  characterAge = 29;
+  printf("He really liked the name %s\n", characterName);
+  printf("but did not like being %d.\n", characterAge);
+
   return 0;
 }
